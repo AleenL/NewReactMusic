efa0b717f907079b72a4514b1616f98a2a9f494e
784757555@qq.com <784757555@qq.com>
2017-09-04 17:07:42 +0800

    修改我喜欢的组件

--- src/SongList.js
+++ src/SongList.js
@@ -1,19 +1,45 @@
 import React from 'react'
+import './SongList.css'
 
 class SongList extends React.Component{
 	render(){
 		return (
-			<div>
-				<div>
-					{this.props.data.map((value,index)=>{
-						return <p key={index}>{((value.song)[0].song[0]).title}</p>
-					})}					
+			<div className='SongList'>
+				<div className='song'>
+					<div className='songListTitle'>
+						<p>试听列表</p>
+						<p></p>
+					</div>
+					<div className='songlist'>
+						{this.props.data.map((value,index)=>{
+							return (
+								<div key={index}>
+									<div>
+										<p>{((value.song)[0].song[0]).title}</p>
+										<p>{((value.song)[0].song[0]).artist}</p>										
+									</div>
+									<p>></p>
+								</div>								
+							)
+						})}							
+					</div>			
 				</div>
-				<div>
-					{this.props.data.map((value,index)=>{
-						if((value.song)[1] !== 'likeBtn') return false;
-						return <p key={index}>{((value.song)[0].song[0]).title}</p>
-					})}					
+				<div className='like'>
+					<div className='likesongTitle'>
+						<p>我喜欢的</p>
+						<p></p>
+					</div>
+					<div className='likesong'>					
+						{this.props.data.map((value,index)=>{
+							if((value.song)[1] !== 'likeBtn') return false;
+							return (
+								<div key={index}>
+									<p>{((value.song)[0].song[0]).title}</p>
+									<p>{((value.song)[0].song[0]).artist}</p>
+								</div>
+							)
+						})}	
+					</div>				
 				</div>				
 			</div>		
 		)
